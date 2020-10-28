50c1e62ee467653201929006cec0b41e546cd4f7
babbage <Milton Castillo>
2020-10-28 06:32:19 +0100

    back and front completely connected

--- hermes_pet_back/functions/src/pets/pet_api.js
+++ hermes_pet_back/functions/src/pets/pet_api.js
@@ -8,4 +8,16 @@ const petsRef = db.collection('pets');
 
 const api = express( crud(petsRef) );
 
+api.put("/like/:id", async (req, res) => {
+    const petRef = petsRef.doc(req.params.id);
+    const snapshot = await petRef.get();
+    const petInfo = snapshot.data();
+    
+    await petRef.update({liked: !petInfo.liked});
+
+    const snapshotUpdated = await petRef.get();
+    const petInfoUpdated = snapshotUpdated.data();
+    res.json({updatedPet: petInfoUpdated});
+})
+
 module.exports = api;
